12ae100382e6bcc10e9ca03bfa45866ff69d417a
Samarth Agarwal <44058680+Thecapable@users.noreply.github.com>
2020-10-01 01:28:55 +0530

    Create Arithmetic Operations.py

---
+++ Arithmetic Operations.py
@@ -0,0 +1,7 @@
+number1=75
+number2=2
+print('Addition=', number1+number2)
+print('Subtraction=', number1-number2)
+print('product=', number1*number2)
+print('Division=', number1/number2)
+print('number1^number2=', number1**number2)
